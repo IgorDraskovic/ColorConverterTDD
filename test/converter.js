38e76c2fbfbf5a641a59521e32ab0271196b248d
IgorDraskovic <idraskov@gmail.com>
2016-06-30 12:11:44 -0400

    converter.js

--- test/converter.js
+++ test/converter.js
@@ -1,3 +1,6 @@
+var expect    = require("chai").expect;
+var converter = require("../app/converter");
+
 describe("Color Code Converter", function() {
 
   describe("RGB to Hex conversion", function() {
